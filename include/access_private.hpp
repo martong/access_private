2a0fe27fecbde85705c9f0cf58bb9dde9bdcd583
Gabor Marton <gabor.marton@prezi.com>
2015-10-18 21:26:55 +0200

    Instead of relying on ADL, declare the friend in namespace scope.

--- include/access_private.hpp
+++ include/access_private.hpp
@@ -3,26 +3,34 @@
 
 namespace detail {
 
-template <typename Tag, typename Tag::ptr_type PtrValue> struct private_cast {
-  friend typename Tag::ptr_type get(Tag) { return PtrValue; }
-};
-
-template <typename Tag, typename PtrType> struct private_cast_helper {
-  using ptr_type = PtrType;
-  friend ptr_type get(Tag);
+template <typename PtrType, PtrType PtrValue, typename TagType>
+struct private_cast {
+  // Normal lookup cannot find in-class defined (inline) friend functions.
+  friend PtrType get(TagType) { return PtrValue; }
 };
 
 } // namespace detail
 
 // @param PtrTypeKind E.g if we have "class A", then it can be "A::*" in case of
 // members, or it can be "*" in case of statics.
+// Class##_##Name is used as a compile-time Tag (type).
 #define _ACCESS_PRIVATE(Class, Type, Name, PtrTypeKind)                        \
   namespace detail {                                                           \
-  using Class##_##Name##_alias = Type;                                         \
-  struct Class##_##Name                                                        \
-      : private_cast_helper<Class##_##Name,                                    \
-                            Class##_##Name##_alias PtrTypeKind> {};            \
-  template struct private_cast<Class##_##Name, &Class::Name>;                  \
+  /* Tag type, used to declare different get funcitons for different members   \
+   */                                                                          \
+  struct Class##_##Name {};                                                    \
+  /* Explicit instantiation */                                                 \
+  template struct private_cast<decltype(&Class::Name), &Class::Name,           \
+                               Class##_##Name>;                                \
+  /* We can build the PtrType only with two aliases */                         \
+  /* E.g. using PtrType = int(int) *; would be illformed */                    \
+  using Alias_##Class##_##Name = Type;                                         \
+  using PtrType_##Class##_##Name = Alias_##Class##_##Name PtrTypeKind;         \
+  /* Declare the friend function, now it is visible in namespace scope. Note,  \
+   * we could declare it inside the Tag type too, in that case ADL would find  \
+   * the declaration. By choosing to declare it here, the Tag type remains a   \
+   * simple tag type, it has no other responsibilities. */                     \
+  PtrType_##Class##_##Name get(Class##_##Name);                                \
   }
 
 #define ACCESS_PRIVATE_FIELD(Class, Type, Name)                                \
